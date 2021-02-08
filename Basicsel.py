6cd56a7eda52fc964b2d20b24a3c703bc27c00d8
amitp988 <amitpatnaik988@gmail.com>
2021-02-08 08:27:34 +0530

    chromedrv

---
+++ Basicsel.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+class Basicsl():
+    def Onsel(self):
+        driver = webdriver.Firefox(executable_path = 'C:/dev/driver/firefox/geckodriver')
+        driver.get("https://learn.letskodeit.com/p/practice")
+aa = Basicsl()
+aa.Onsel()
---
+++ selmn.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+class Selmn():
+        def test(self):
+                driver = webdriver.Chrome(executable_path="C:\\dev\\driver\\chrome\\chromedrive.exe")
+                driver.get("https://learn.letskodeit.com/p/practice")
+ee = Selmn()
+ee.test
