b01be0f940bd8bb1076b80be0fa5c5ad316adc5d
amitp988 <amitpatnaik988@gmail.com>
2021-02-08 08:38:05 +0530

    crm

--- selmn.py
+++ selmn.py
@@ -1,7 +1,8 @@
 from selenium import webdriver
-class Selmn():
+class Rmselmn():
         def test(self):
                 driver = webdriver.Chrome(executable_path="C:\\dev\\driver\\chrome\\chromedrive.exe")
-                driver.get("https://learn.letskodeit.com/p/practice")
-ee = Selmn()
+                driver.get("https://www.letskodeit.com/")
+                time.sleep(100)
+ee = Rmselmn()
 ee.test
