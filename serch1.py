b317ac4bc86d6b0487c80d9154e49889a114e935
amitp988 <amitpatnaik988@gmail.com>
2021-02-09 07:51:04 +0530

    updatewithclass

--- serch1.py
+++ serch1.py
@@ -1,8 +1,11 @@
 from selenium import webdriver
-
-x = input('Enter The Name:')
-if x == "Sachin Tendulkar":
-    driver = webdriver.Firefox(executable_path='C:/dev/driver/firefox/geckodriver')
-    driver.get('https://en.wikipedia.org/wiki/Sachin_Tendulkar')
-else:
-    print(x)
+class Serachname():
+    def Sachin(self):
+        x = input('Enter The Name:')
+        if x == "Sachin Tendulkar":
+            driver = webdriver.Firefox(executable_path='C:/dev/driver/firefox/geckodriver')
+            driver.get('https://en.wikipedia.org/wiki/Sachin_Tendulkar')
+        else:
+            print(x)
+qw = Serachname()
+qw.Sachin()
