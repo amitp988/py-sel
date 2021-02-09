25e834af7e7744b3d9dc1622a8398c3af1ee3aa0
amitp988 <amitpatnaik988@gmail.com>
2021-02-09 07:14:27 +0530

    ruslt1

---
+++ serch1.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+
+x = input('Enter The Name:')
+if x == "Sachin Tendulkar":
+    driver = webdriver.Firefox(executable_path='C:/dev/driver/firefox/geckodriver')
+    driver.get('https://en.wikipedia.org/wiki/Sachin_Tendulkar')
+else:
+    print(x)
