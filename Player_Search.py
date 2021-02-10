f08da43d5090a21565c3c54730ae483f0f96ff52
amitp988 <amitpatnaik988@gmail.com>
2021-02-10 09:00:55 +0530

    pl

---
+++ Player_Search.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.support.select import Select
+import time
+driver = webdriver.Chrome(executable_path="C:\\dev\\driver\\chrome\\chromedriver.exe")
+driver.get("https://www.cricbuzz.com/")
+driver.implicitly_wait(10)
+search_bar = driver.find_element_by_id("search_bar")
+search_bar.click()
+search_bar.send_keys('Sachin Tendulkar')
+search_btn = driver.find_elements_by_link_text("SEARCH")
+search_btn.click()
